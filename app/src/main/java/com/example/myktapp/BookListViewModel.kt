4195068a1b0e1be5176965db1360468923ee19b4
Habib Babayev <babayevh348@gmail.com>
2025-06-06 20:31:55 +0400

    yeni deyisiklikler

--- app/src/main/java/com/example/myktapp/BookListViewModel.kt
+++ app/src/main/java/com/example/myktapp/BookListViewModel.kt
@@ -1,9 +1,5 @@
 package com.example.myktapp
 
-import androidx.compose.runtime.MutableState
-import androidx.lifecycle.ViewModel
-import com.example.myktapp.component.BookInfo
-
 //class BookListViewModel: ViewModel() {
 //    private val viewBooks: MutableState<List<BookInfo>>=mutableStateOf(emptylist())
-//}
+//}/////////////
